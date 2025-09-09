37e49d48ec251135a1352264ca1eaa4ae40ee832
Henny Sluyter-Gäthje <sluytergaeth@uni-potsdam.de>
2025-09-09 16:35:11 +0200

    remove old files, add author to license

--- openapi-generated-wrapper/openapi-docker.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -v "$PWD:/local" openapitools/openapi-generator-cli generate \
-                                                                             -i https://dracor.org/api/v1/openapi.yaml \
-                                                                             -g python \
-                                                                             -o /local/auto-generated
