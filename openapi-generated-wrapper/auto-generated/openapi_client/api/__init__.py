8ee2d89defb486e5c2a52bcaa3f0cb3617111b8d
Henny Sluyter-Gäthje <sluytergaeth@uni-potsdam.de>
2025-09-09 15:14:48 +0200

    delete generated wrapper, saved in pydracor-base

--- openapi-generated-wrapper/auto-generated/openapi_client/api/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.admin_api import AdminApi
-from openapi_client.api.public_api import PublicApi
-from openapi_client.api.webhook_api import WebhookApi
-from openapi_client.api.wikidata_api import WikidataApi
-
